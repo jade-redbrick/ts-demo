52bebff624a5813b36c54f308f95e756583b5ea3
Jade <jade@redbrick.space>
2022-11-29 18:42:09 +0900

    Add: DashBoard TotalSection Component

---
+++ src/DashBoard/TotalSection/index.tsx
@@ -0,0 +1,12 @@
+import SectionCard from "./SectionCard";
+
+function TotalSection() {
+  return (
+    <div>
+      <SectionCard type="revenue" content={32000} />
+      <SectionCard type="view" content={32000} />
+    </div>
+  );
+}
+
+export default TotalSection;
